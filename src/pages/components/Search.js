fd2ac3f95e8fdbe18619f6bc33f0bb2a3fd3e311
mirabelanegotei <mirabelanegotei@gmail.com>
2024-11-11 15:44:10 +0200

    Implement search page with input field and results list

--- src/pages/components/Search.js
+++ src/pages/components/Search.js
@@ -3,8 +3,15 @@ import styles from "../../styles/Search.module.css";
 const Search = () =>{
     return (
         <div className={styles.container}>
-            <h1>Welcome to SearchPage</h1>
-        </div>
-    )
-}
+          <h1 className={styles.title}>Welcome to Search Page</h1>
+          <h2 className={styles.title}>Search for cities name</h2>
+          <form className={styles.form}>
+            <input className={styles.input} type="search" placeholder="Search here ..." />
+            <i className={`fas fa-search ${styles.icon}`} ></i> 
+          </form>
+          <ul  className={styles.resultsList}>
+           <li className={styles.noResults}>No results found</li>
+          </ul>
+        </div>)}
+
 export default Search;
